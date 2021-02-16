ddeddb7e04551f8de7b9f57ed229a09d330cdbc8
iNeedHelpX <jpak0444@gmail.com>
2021-02-16 17:02:47 -0500

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,17 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:skipapilogin/main.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-=======
 import 'package:dashapi/pages/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -24,7 +13,6 @@ void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(LoginPage());
->>>>>>> changed api calls
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
